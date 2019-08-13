66169c03e8c879e19872d42508b307e07ba2e3a7
Patric Pförtner <ai.online.shop.2019@gmail.com>
2019-08-13 15:06:30 +0300

    hi

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -78,8 +78,76 @@
 
 @slot('content')
 <div class="container">
-    <h2 class="m10">Something big is coming soon! Stay tuned...</h2>
-
+  <amp-animation id="blackmirror"
+layout="nodisplay">
+<script type="application/json">
+  {
+  "duration": "0.2s",
+  "fill": "both",
+  "direction": "alternate",
+    "selector": "#blackmirror",
+    "animations": [{
+      "keyframes": {
+        "opacity": [1, 0.9, 1, 0.5, 0.1]
+      }
+    }]
+  }
+</script>
+</amp-animation>
+<amp-position-observer  on="scroll:blackmirror.seekTo(percent=event.percent)" layout="nodisplay"></amp-position-observer>
+  <div class="overflow-container">
+    <div id="blackmirror">
+      <amp-img alt="Ai-Online-Shop.com" class="mobile layer-black" layout="responsive" srcset="
+      {{ asset('assets/img/legal/impressum-1/200.webp') }} 200w,
+      {{ asset('assets/img/legal/impressum-1/495.webp') }} 495w,
+      {{ asset('assets/img/legal/impressum-1/689.webp') }} 689w,
+      {{ asset('assets/img/legal/impressum-1/881.webp') }} 881w,
+      {{ asset('assets/img/legal/impressum-1/1000.webp') }} 1000w" src="{{ asset('assets/img/legal/impressum-1/1000.webp') }}" height="1298" width="1000">
+    <amp-img fallback alt="Ai-Online-Shop.com" class="mobile layer-black" layout="responsive" srcset="
+    {{ asset('assets/img/legal/impressum-1/200.jpg') }} 200w,
+    {{ asset('assets/img/legal/impressum-1/495.jpg') }} 495w,
+    {{ asset('assets/img/legal/impressum-1/689.jpg') }} 689w,
+    {{ asset('assets/img/legal/impressum-1/881.jpg') }} 881w,
+    {{ asset('assets/img/legal/impressum-1/1000.jpg') }} 1000w" src="{{ asset('assets/img/legal/impressum-1/1000.jpg') }}" height="1298" width="1000">
+    </amp-img>
+    </amp-img>
+    <amp-img alt="Ai-Online-Shop.com" class="desktop layer-black" layout="responsive" srcset="
+    {{ asset('assets/img/legal/impressum-2/200.webp') }} 200w,
+    {{ asset('assets/img/legal/impressum-2/505.webp') }} 505w,
+    {{ asset('assets/img/legal/impressum-2/705.webp') }} 705w,
+    {{ asset('assets/img/legal/impressum-2/905.webp') }} 905w,
+    {{ asset('assets/img/legal/impressum-2/1024.webp') }} 1024w" src="{{ asset('assets/img/legal/impressum-2/1024.webp') }}" height="512" width="1024">
+    <amp-img fallback alt="Ai-Online-Shop.com" class="desktop layer-black" layout="responsive" srcset="
+    {{ asset('assets/img/legal/impressum-2/200.jpg') }} 200w,
+    {{ asset('assets/img/legal/impressum-2/505.jpg') }} 505w,
+    {{ asset('assets/img/legal/impressum-2/705.jpg') }} 705w,
+    {{ asset('assets/img/legal/impressum-2/905.jpg') }} 905w,
+    {{ asset('assets/img/legal/impressum-2/1024.jpg') }} 1024w" src="{{ asset('assets/img/legal/impressum-2/1024.jpg') }}" height="512" width="1024">
+    </amp-img></amp-img>
+</div>
+<amp-animation id="blogphototext"
+layout="nodisplay">
+<script type="application/json">
+{
+"duration": "0.2s",
+"fill": "both",
+"direction": "alternate",
+  "selector": "#blogphototext",
+  "animations": [{
+    "keyframes": {
+      "opacity": [1, 0.9, 1, 0.4],
+      "transform": ["translateY(0px)","translateY(3px)","translateY(10px)","translateY(150px)"]
+    }
+  }]
+}
+</script>
+</amp-animation>
+<amp-position-observer  on="scroll:blogphototext.seekTo(percent=event.percent)" layout="nodisplay"></amp-position-observer>
+    <div class="text-container" id="blogphototext">
+      <p class="p-section">Ai-Online-Shop.com</p>
+    <h1 class="header-text">Something Big is About to Come....</h1>
+    </div>
+  </div>
 </div>
 @endslot
 @endcomponent
